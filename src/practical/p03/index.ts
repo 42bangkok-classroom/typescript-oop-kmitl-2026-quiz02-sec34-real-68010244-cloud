309851ff206f58ce80dd82f30119491f2b459de8
68010244@kmitl.ac.th <68010244@kmitl.ac.th>
2026-01-17 04:27:36 +0700

    2

--- src/practical/p03/index.ts
+++ src/practical/p03/index.ts
@@ -1,44 +1,9 @@
-import axios from "axios";
-interface Geo {
-  lat: string;
-  lng: string;
-}
-interface Address {
-  street: string;
-  suite: string;
-  city: string;
-  zipcode: string;
-  geo: Geo;
-}
-interface ApiUser {
-  id: number;
-  name?: string;
-  phone?: string;
-  address?: Address;
-}
-interface UserOutput {
-  id: number;
-  name: string | null;
-  phone: string | null;
-  address: Address | null;
-}
-export async function filterUserById(id: number): Promise<UserOutput | string> {
-  try {
-    const url = 'https://jsonplaceholder.typicode.com/users';
-    const response = await axios.get<ApiUser[]>(url);
-    const data = response.data;
-    const us = data.find((u) => u.id === id);
+import { getPostalAddress } from "../p01";
+export async function filterUserById(id: number) {
+    const rs = await getPostalAddress();
+    const us = rs.find((u) => u.id === id);
     if (!us) {
       return "Invalid id";
     }
-    const result = {
-      id: us.id,
-      name: us.name ?? null,
-      phone: us.phone ?? null,
-      address: us.address ?? null,
-    };
-    return result;
-  } catch (error) {
-    return "Invalid id";
-  }
+    return us;
 }
